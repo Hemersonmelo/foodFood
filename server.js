c26d9da3d3ae403bb1c800b7dcfeda414d358949
Hemerson Melo <hemerson.c.melo@gmail.com>
2021-08-20 17:40:34 -0300

    alteração send

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (Request, response) => {
-    response.send('helo world');
+    response.send(index.html);
 });
 
 //const PORT = process.env.PORT || 5000;
