52832e3ceadccb74f1facee5848a55ee0ae7c555
Hemerson Melo <hemerson.c.melo@gmail.com>
2021-08-20 19:17:12 -0300

    teste

--- server.js
+++ server.js
@@ -8,5 +8,21 @@ app.get("/", (Request, response) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(process.env.PORT || 5000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+
+const getExtension = fileName => (
+    fileName.substr(fileName.lastIndexOf('.') + 1)
+)
+
+app.get('*', ({ url }, res) => {
+    let filePath
+    if (['js', 'css', 'png', 'jpg'].includes(getExtension(url))) {
+        filePath = __dirname + '/public/' + url
+    } else {
+        filePath = __dirname + '/public/' + url + '/index.html'
+    }
+    res.sendFile(filePath)
+    console.log(url);
 });
