f038dc676009096c0a2d24174c76e8d218dbd01b
Hemerson Melo <hemerson.c.melo@gmail.com>
2021-08-20 17:35:33 -0300

    port

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get("/", (Request, response) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log("listning on port" + PORT);
-})
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
