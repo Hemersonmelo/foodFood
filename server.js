4f73d0f8f16c27689ff2c1e262a77361c5d5b409
Hemerson Melo <hemerson.c.melo@gmail.com>
2021-08-20 23:04:06 -0300

    xuxu

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const http = require('https');
-const express = require('express');
+const express = require("express");
+const app = express();
 const path = require('path');
 
-const app = express();
-
-let getData = () => {
-    //O seu método de leitura do arquivo vem aqui
-    return 'qualquer que seja o seu resultado aqui';
-}
-
-app.get('/data', (req, res) => {
-    res.send(getData());
-});
-
-app.get('/', (req, res) => {
+app.get("/", (Request, response) => {
+    // response.sendFile("index.html")
     console.log(path.join(__dirname + '/index.html'))
-    res.sendFile(path.join(__dirname + '/index.html'));
+        //response.sendFile(path.join(__dirname + '/index.html'));
+    response.send('oi xuxu')
 });
 
-http.createServer(app).listen(8080, () => {
-    console.log('server funcionando');
+const PORT = process.env.PORT || 5000;
+
+app.listen(process.env.PORT || 5000, function() {
+    //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+
+const getExtension = fileName => (
+    fileName.substr(fileName.lastIndexOf('.') + 1)
+)
+
+app.get('*', ({ url }, res) => {
+    let filePath
+    if (['js', 'css', 'png', 'jpg'].includes(getExtension(url))) {
+        filePath = __dirname + '/public/' + url
+    } else {
+        filePath = __dirname + '/public/' + url + '/index.html'
+    }
+    res.sendFile(filePath)
+    console.log(url);
 });
