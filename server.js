23f7f4991ec645cbff217b6a992ca4b14995e3c7
Hemerson Melo <hemerson.c.melo@gmail.com>
2021-08-25 20:40:27 -0300

    ok

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'))
-    //const path = require('path');
 
 app.get("/", (Request, response) => {
-    response.sendFile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/public/index.html');
 });
 
-//const PORT = process.env.PORT || 3333;    
-app.listen(process.env.PORT || 3333, () => {
+app.listen(process.env.PORT || 8080, () => {
     //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
