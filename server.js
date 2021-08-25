5db4f96b82253d1928a7e7a4042d040e8c2fa969
Hemerson Melo <hemerson.c.melo@gmail.com>
2021-08-25 20:21:38 -0300

    ok

--- server.js
+++ server.js
@@ -1,26 +1,7 @@
-/*var http = require('http');
-//const { error } = require('console');
-var fs = require('fs');
-
-function onRequest(Request, response) {
-    response.writeHead(200, { 'content-type': 'text/htm' });
-    fs.readFile('./index.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('file not found!');
-        } else {
-            response.write(data);
-        }
-        response.end()
-
-    });
-}
-
-http.createServer(onRequest).listner(8080);
-*/
 const express = require('express');
 const app = express();
-//const path = require('path');
+app.use(express.static('public'))
+    //const path = require('path');
 
 app.get("/", (Request, response) => {
     response.sendFile(__dirname + '/index.html');
