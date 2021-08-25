ba9dd2a740c6d1ebe2fa36107f032c0bc53479f4
Hemerson Melo <hemerson.c.melo@gmail.com>
2021-08-25 20:59:03 -0300

    ok

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get("/", (Request, response) => {
     response.sendFile(__dirname + '/public/index.html');
